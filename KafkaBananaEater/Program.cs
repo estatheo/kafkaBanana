53b04c7b68a25d9b41f2e2012ac92bc42a4878ba
estatheo <theodor.chichirita@gmail.com>
2019-03-20 17:07:00 +0000

    first commit

---
+++ KafkaBananaEater/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace KafkaBananaEater
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Threading;
+    using Confluent.Kafka;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "banana-consumers",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe("banana-topic");
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true) 
+                    {
+                        try 
+                        {
+                            var cr = consumer.Consume(cts.Token);
+                            Console.WriteLine(cr.Value);
+                        }
+                        catch ( ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    consumer.Close();
+                }
+            }
+        }
+    }
+}
---
+++ KafkaBananaStore/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace KafkaBananaStore
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Confluent.Kafka;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092"};
+
+            Action<DeliveryReport<Null, string>> handler = r =>
+                Console.WriteLine(!r.Error.IsError
+                ? $"Delivered message to {r.TopicPartitionOffset}"
+                : $"Delivery Error: {r.Error.Reason}");
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                var stringValue = "";
+                for (int i=0; i<100; ++i){
+                    stringValue += "🍌";
+                    producer.BeginProduce("banana-topic", new Message<Null, string> { Value = stringValue}, handler);
+                }
+
+                producer.Flush(TimeSpan.FromSeconds(10));
+            }    
+        }
+    }
+}
